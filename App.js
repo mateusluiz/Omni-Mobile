9e4b0f4d661c2ee7c6be6c0470a6e7831d745115
mateusluiz <mateus74890@gmail.com>
2020-03-02 01:45:24 -0300

    Inclusão de ambiente Mobile

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello World!!!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title: {
+    fontWeight: 'bold',
+    color: '#fff'
+  },
+});
